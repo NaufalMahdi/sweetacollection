b6652ec091861dfe518f8d954495d93379a26673
Monokuro <07akhmadnur@gmail.com>
2022-05-02 18:30:36 +0700

    Fixing item list in admin page + change database url
    database url changed to localhost (temporary)

--- components/Cards/CardProduct.js
+++ components/Cards/CardProduct.js
@@ -1,39 +1,39 @@
 import React from "react";
 
-const CardProduct = ({ item }) => {
+const CardProduct = ({ data }) => {
   return (
     <>
       {/* <div className="h-24 w-24 mx-auto my-auto bg-white"></div> */}
-      <div className="max-w-lg h-fit max-h-fit shadow-md bg-white rounded-lg overflow-hidden m-4">
+      <div className="max-w-sm sm:max-w-md lg:max-w-sm h-fit max-h-fit shadow-md bg-white rounded-lg overflow-hidden mb-5">
         <div className="grid grid-cols-3 xs:grid-cols-1 gap-0">
           <div className="">
             <img
               className="w-full h-full object-cover md:h-full"
-              src="/img/products/sample.jpg"
-              alt="Man looking at item at a store"
+              src={"/img/products/" + data.image}
+              alt={data.product_name}
             ></img>
           </div>
           <div className="p-8 h-full overflow-hidden col-span-2">
-            <a
-              href="#"
-              className="mt-1 text-lg leading-tight font-medium text-black hover:underline"
-            >
-              Finding customers for your new business
-            </a>
-            <p className="mt-2 w-[100%] h-full text-slate-500 truncate">
-              Getting a new business off the ground is a lot of hard work. Here
-              are five ideas you can use to find your first customers. Getting a
-              new business off the ground is a lot of hard work. Here are fiv
-              ideas you can use to find your first customers. Getting a new
-              business off the ground is a lot of hard work. Here are five ideas
-              you can use to find your first customers. Getting a new business
-              off the ground is a lot of hard work. Here are five ideas you can
-              use to find your first customers. Getting a new business off the
-              ground is a lot of hard work. Here are five ideas you can use to
-              find your first customers. Getting a new business off the ground
-              is a lot of hard work. Here are five ideas you can use to find
-              your first customers.
-            </p>
+            <div className="flex flex-col h-full">
+              <div className="basis-1/2">
+                <a
+                  href="#"
+                  className="mt-1 text-lg leading-tight font-medium text-black hover:underline"
+                >
+                  {data.product_name}
+                </a>
+                <p className="mt-2 w-full h-full text-slate-500 truncate">
+                  {data.description}
+                </p>
+              </div>
+              <div className="relative basis-1/2">
+                <div className="absolute bottom-0 right-0">
+                  <button className="text-red-500 active:text-red-600  outline-none focus:outline-none ease-linear transition-all duration-150">
+                    <i class="fa-solid fa-trash-can"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- pages/admin/products/index.js
+++ pages/admin/products/index.js
@@ -8,31 +8,84 @@ import Admin from "layouts/Admin";
 import CardProduct from "components/Cards/CardProduct";
 
 const products = ({ data }) => {
-  const [name, setName] = useState([]);
-  const [loading, setLoading] = useState(false);
   let view;
-  let title = "aaaa";
-  if (loading) {
-    view = <>Please Wait</>;
-  } else {
-    view = (
-      <>
-        <div className="relative bg-transparent pt-20">
-          <div className="px-6 mt-10 h-screen">
-            <div className="flex">
-              <CardProduct item={title} />
-              <CardProduct item={title} />
-              <CardProduct item={title} />
+  try {
+    if (data) {
+      view = (
+        <>
+          <div className="relative bg-transparent pt-20 mx-5">
+            <div className="mt-10 h-screen">
+              <div className="w-full">
+                <div className="flex flex-row-reverse">
+                  <button
+                    className="bg-blueGray-500 text-white active:bg-blueGray-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
+                    type="button"
+                  >
+                    <div className="flex flex-nowrap justify-between">
+                      <div className="mx-auto my-auto">
+                        <i class="fa-solid fa-circle-plus"></i>
+                        <span> Buat</span>
+                      </div>
+                    </div>
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex flex-wrap justify-between mt-5">
+                {data.length > 0
+                  ? data.map((val) => <CardProduct data={val} />)
+                  : "a"}
+              </div>
             </div>
           </div>
-        </div>
-      </>
-    );
+        </>
+      );
+    } else {
+      view = <>Please Wait</>;
+    }
+  } catch (err) {
+    view = <>Gagal memuat data</>;
   }
+  // useEffect(() => {
+  //   try {
+  //     if (loading) {
+  //       view = <>Please Wait</>;
+  //     } else {
+  //       view = (
+  //         <>
+  //           <div className="relative bg-transparent pt-20">
+  //             <div className="px-6 mt-10 h-screen">
+  //               <div className="flex">{data}</div>
+  //             </div>
+  //           </div>
+  //         </>
+  //       );
+  //     }
+  //   } catch (err) {
+  //     view = <>ERROR {err}</>;
+  //   } finally {
+  //     loading = false;
+  //   }
+  // });
 
   return view;
 };
 
+const getStaticProps = async () => {
+  let data;
+  await axios
+    .get("http://localhost:3000/api/admin/product/getAllProducts", {})
+    .then((res) => {
+      data = res.data.data;
+    });
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
+
 products.layout = Admin;
 
+export { getStaticProps };
 export default products;
