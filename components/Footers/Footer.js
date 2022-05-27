b979de6a1a89dc684b64274e518638b698332afc
NaufalMahdi <71993847+NaufalMahdi@users.noreply.github.com>
2022-05-27 16:08:25 +0700

    update

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -26,9 +26,9 @@ export default function Footer() {
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap text-center lg:text-left">
             <div className="w-full lg:w-6/12 px-4">
-              <h4 className="text-3xl font-semibold">Let's keep in touch!</h4>
+              <h4 className="text-3xl font-semibold">Hubungi Kami</h4>
               <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-                Find us on any of these platforms, we respond 1-2 business days.
+                Kalian bisa menghubungi kami lewat :
               </h5>
               <div className="mt-6 lg:mb-0 mb-6">
                 <button
@@ -106,7 +106,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                        href="https://github.com/creativetimofficial/notus-nextjs/blob/main/LICENSE.md?ref=nnjs-footer"
+                        href="https://github.com/NaufalMahdi/sweetacollection"
                       >
                         MIT License
                       </a>
